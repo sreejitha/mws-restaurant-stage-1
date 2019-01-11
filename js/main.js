3e840a3014c867babf47fa9c99359199e9701d15
sreejitha <sreejitha.vijayan@gmail.com>
2019-01-11 12:09:15 -0800

    feat: added labels for select

--- js/main.js
+++ js/main.js
@@ -179,7 +179,9 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  more.setAttribute('role', 'button');
+  more.setAttribute('aria-label', 'View ' + restaurant.name + ' Restaurant Details');
+  li.append(more);
 
   return li
 }

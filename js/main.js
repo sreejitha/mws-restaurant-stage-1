150fafa24f38e40d674d7afa758d0ab5796c720f
sreejitha <sreejitha.vijayan@gmail.com>
2019-01-16 14:59:50 -0800

    style:format for code readability

--- js/main.js
+++ js/main.js
@@ -11,8 +11,22 @@ document.addEventListener('DOMContentLoaded', (event) => {
   initMap(); // added
   fetchNeighborhoods();
   fetchCuisines();
+  registerServiceWorker();
 });
 
+/*code for below function taken from instructor notes */
+registerServiceWorker = () => {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/serviceworker.js', {
+    scope: '/'}).then(function() {
+        console.log('service worker registered!');
+      }).catch(function() {
+        console.log('registration failed');
+      });
+    });
+  }
+}
 /**
  * Fetch all neighborhoods and set their HTML.
  */
@@ -71,23 +85,23 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
 /**
  * Initialize leaflet map, called from HTML.
  */
- initMap = () => {
-   self.newMap = L.map('map', {
-         center: [40.722216, -73.987501],
-         zoom: 12,
-         scrollWheelZoom: false
-       });
-   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-     mapboxToken: 'pk.eyJ1Ijoic3JlZWppdGhhIiwiYSI6ImNqcWxqZzhzaTEybWk0OHF2OTFzYmZ2b2IifQ.aSmTd3uQqmuxQmgKWjbWzQ',
-     maxZoom: 18,
-     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-       'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-     id: 'mapbox.streets'
-   }).addTo(newMap);
+initMap = () => {
+  self.newMap = L.map('map', {
+    center: [40.722216, -73.987501],
+    zoom: 12,
+    scrollWheelZoom: false
+  });
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
+    mapboxToken: 'pk.eyJ1Ijoic3JlZWppdGhhIiwiYSI6ImNqcWxqZzhzaTEybWk0OHF2OTFzYmZ2b2IifQ.aSmTd3uQqmuxQmgKWjbWzQ',
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox.streets'
+  }).addTo(newMap);
 
-   updateRestaurants();
- }
+  updateRestaurants();
+}
 /* window.initMap = () => {
   let loc = {
     lat: 40.722216,
@@ -194,6 +208,7 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     // Add marker to the map
     const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.newMap);
     marker.on("click", onClick);
+
     function onClick() {
       window.location.href = marker.options.url;
     }
